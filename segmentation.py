eb2cdcf90f168f1baa448abd750e92b07a4d4980
Aswin Vasudevan21 <aswinv@bu.edu>
2020-06-05 11:50:40 -0400

    consume data from local

--- segmentation.py
+++ segmentation.py
@@ -1,7 +1,14 @@
 from perception import maskr as m
 from perception import angledataset as a
 from ResultAnalyser import ResultAnalyzer
+import matplotlib
+matplotlib.use('agg')
+
+
 import matplotlib.pyplot as plt
+import argparse
+
+from dataset import DatasetFromFile
 
 class segmentation:
     def __init__(self):
@@ -16,11 +23,12 @@ class segmentation:
         ax.set_title("Original")
         plt.show()
 
-    def trainMaskRCNN(self,train,val,epochs):
+    def trainMaskRCNN(self,epochs,dataset):
         maskrcnn = m.MaskR()
-        #change to npy consume
-        train, val, test = a.AngleDataset.createSets()
-        maskrcnn.train(train, val, epochs=epochs)
+        path = dataset + "/"
+        train_file = DatasetFromFile(path + "train_0.npz").load_from_file()
+        val_file   = DatasetFromFile(path + "val_0.npz").load_from_file()
+        maskrcnn.train(train_file, val_file, epochs=epochs)
         return maskrcnn
 
     def predictMaskRCNN(self,ANY_INDEX,test,maskrcnn,model_path,weight_path):
@@ -38,16 +46,54 @@ class segmentation:
         from sklearn.metrics import mean_squared_error
         mean_squared_error(y_test, y_pred[0])
 
-segmentation_pipeline = segmentation()
-result = segmentation_pipeline.trainMaskRCNN(60000,20000,1000)
+    def testTrainedModel(self,dataset,weights_path):
+        segmentation_pipeline = segmentation()
+        path = dataset + "/"
+        test = DatasetFromFile(path + "test_0.npz").load_from_file()
+        maskrcnn = result_analyzer.LoadModelfromLocal(model_path, weights_path)
+        segmentation_pipeline.predictMaskRCNN(10, maskrcnn, test, model_path, weights_path)
+
+def main():
+    print("-----LEVEL TRAINING PIPELINE-----")
+    parser = argparse.ArgumentParser("level")
+    parser.add_argument("dataset", help="dataset can be any one of:  angle or area or curvature or direction or length or position_common_scale or position_non_aligned_scale or volume ", type=str)
+    parser.add_argument("path", help=" Path to store the model", type=str)
+    parser.add_argument("epoch", help="val data count", type=int)
+    args = parser.parse_args()
+    dataset_path=''
+    if args.dataset == "angle":
+        dataset_path = args.path + '/' + 'angle'
+    elif args.dataset == "area":
+        dataset_path = args.path + '/' + 'area'
+    elif args.dataset == "curvature":
+        dataset_path = args.path + '/' + 'curvature'
+    elif args.dataset == "direction":
+        dataset_path = args.path + '/' + 'direction'
+    elif args.dataset == "length":
+        dataset_path = args.path + '/' + 'length'
+    elif args.dataset == "position_common_scale":
+        dataset_path = args.path + '/' + 'position_common_scale'
+    elif args.dataset == "position_non_aligned_scale":
+        dataset_path = args.path + '/' + 'position_non_aligned_scale'
+    elif args.dataset == "volume":
+        dataset_path = args.path + '/' + 'volume'
+
+    segmentation_pipeline = segmentation()
+    result = segmentation_pipeline.trainMaskRCNN(args.epoch, dataset_path)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
 
 #save model
 result_analyzer = ResultAnalyzer()
 model_path='/models'
-result_analyzer.saveModels("segementationMaskRCNN",result,model_path,1000)
+#result_analyzer.saveModels("segementationMaskRCNN",result,model_path,1000)
 
 #load model and predict
-weights_path =""
-maskrcnn = result_analyzer.LoadModelfromLocal(model_path,weights_path)
-train, val, test = a.AngleDataset.createSets()
-segmentation_pipeline.predictMaskRCNN(10,maskrcnn,test,model_path,weights_path)
